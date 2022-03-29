da9fed3a94752de5b9e1648b8053900f89aac5a2
liselen97 <liselenpannella@gmail.com>
2022-03-29 19:30:28 -0300

    post request

--- script.js
+++ script.js
@@ -13,8 +13,24 @@ function initialization() {
     $("button").on("click", function () {
         if (ratingNumber != null) {
             $("h3 span").text(ratingNumber);
+
+            $.ajax({
+                type: "POST",
+                url: "https://subteweb.gatsbyjs.io/api/challenge",
+                data: {
+                  "rating": ratingNumber,
+                },
+                success: function (response) {
+                    
+                    console.log(response);
+                },
+                dataType: "json"
+            });
+
             $("#screen1").hide();
             $("#screen2").show();
+
+           
         }
     })
 }
